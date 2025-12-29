ab8c38d39edcb0afe469854df0c2d77161032f34
rhysje00 <rhysjevans00@gmail.com>
2025-12-29 01:59:42 +0000

    Deploying to gh-pages from @ rhysje00/rhysje00.github.io@6779ce6ef12d8dd8e19e90819d46f6c854f77649 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,7 +59,7 @@ ninja.data = [{
               window.location.href = "/projects/graphsym/";
             },},{id: "projects-other-data",
           title: 'Other data',
-          description: "Other objects I have computed",
+          description: "Collections of miscellaneous objects",
           section: "Projects",handler: () => {
               window.location.href = "/projects/misc_data/";
             },},{id: "projects-neumaier-graphs",
